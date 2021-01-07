fd64de6eb5de2b777b241c0ea448a0ca9f7964f0
Johan Castillo <jcjohan2707@gmail.com>
2021-01-07 12:02:39 -0400

    Request GET with axios

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Home extends Component{
+    
+    componentDidMount(){
+        axios.get('https://dog.ceo/api/breeds/image/random')
+        .then(
+            response => console.log(response.data)
+        ).catch(
+            error => console.log(error)
+        )
+    }
+    
     render(){
         return (
             <h1>Hello World</h1>
