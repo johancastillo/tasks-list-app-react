7f3c8967f6ff3493c2c57e75edfeda1815814eb6
Johan Castillo <jcjohan2707@gmail.com>
2021-01-07 12:15:33 -0400

    Render object of the request GET in the DOM

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class Home extends Component{
+
+    state = {
+        title: 'My Client App',
+        dogs: []
+    }
     
     componentDidMount(){
         axios.get('https://dog.ceo/api/breeds/image/random')
         .then(
-            response => console.log(response.data)
+            response => {
+                this.setState({ dogs: [response.data] })
+                console.log(response.data)
+            }
         ).catch(
             error => console.log(error)
         )
@@ -14,7 +22,15 @@ class Home extends Component{
     
     render(){
         return (
-            <h1>Hello World</h1>
+            <>
+                <h1>{ this.state.title }</h1>
+                
+                {this.state.dogs.map(
+                    e => <img src={e.message} alt="Dog" />
+                )}
+
+                
+            </>
         )
     }
 }
